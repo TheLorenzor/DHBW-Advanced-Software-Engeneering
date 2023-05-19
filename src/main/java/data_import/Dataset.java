16c76aad42553b0cf5d40244b55c7ba67004aca1
Tilmann <tilmann.lorenz@gmx.de>
2023-05-19 18:56:42 +0200

    started tests

---
+++ src/main/java/data_import/Dataset.java
@@ -0,0 +1,6 @@
+package data_import;
+
+public interface Dataset {
+    public void loadDataset();
+    public double getDistance(String point1,String point2);
+}
---
+++ src/main/java/data_import/SmallDatasetLoader.java
@@ -0,0 +1,13 @@
+package data_import;
+
+public class SmallDatasetLoader implements Dataset{
+    @Override
+    public void loadDataset() {
+
+    }
+
+    @Override
+    public double getDistance(String point1, String point2) {
+        return 0;
+    }
+}
