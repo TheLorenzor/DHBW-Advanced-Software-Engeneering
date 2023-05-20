c03f3ed97fe4ba40a00239342a09da6dd2f5d744
Tilmann <tilmann.lorenz@gmx.de>
2023-05-20 13:01:16 +0200

    loads all the ids in

---
+++ src/main/java/data_import/Constants.java
@@ -0,0 +1,12 @@
+package data_import;
+
+public enum Constants {
+    StationNode,
+    CustomerNode,
+    Depot;
+    final double max_tank= 60;
+    final double consumption = 0.2;
+    final double tour_length = 11;
+    final double velocity = 40;
+    final int vehicles = 20;
+}
--- src/main/java/data_import/SmallDatasetLoader.java
+++ src/main/java/data_import/SmallDatasetLoader.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -13,6 +14,8 @@ import java.util.stream.Stream;
 public class SmallDatasetLoader implements Dataset{
     private Random random;
     private boolean is_open;
+
+    private HashMap<String,Constants> ids;
     public SmallDatasetLoader() {
         random = new Random();
         is_open = false;
@@ -32,21 +35,54 @@ public class SmallDatasetLoader implements Dataset{
         // get a random small path
         path_to_dataset = path_to_dataset.resolve(names.get(random.nextInt(0,names.size())));
         File dataset = new File(path_to_dataset.toUri());
-        ArrayList<Object[]> list = new ArrayList<>();
+        ArrayList<Object[]> list = new ArrayList<>(25);
         try {
             BufferedReader reader = new BufferedReader(new FileReader(dataset));
             String dataset_line = reader.readLine();
             dataset_line =reader.readLine();
             while(dataset_line!=null) {
+                Object[] line_info = new Object[4];
+                int end_tab = dataset_line.indexOf('\t');
+                if (end_tab>-1) {
+                    // first one is ID
+                    String information =dataset_line.substring(0,end_tab);
+                    line_info[0] = information;
+                    int start_tab = end_tab+1;
+                    // add another one to skip the first tab
+                    end_tab = dataset_line.indexOf('\t',start_tab);
+                    information = dataset_line.substring(start_tab,end_tab);
+                    switch (information) {
+                        case "c" -> line_info[1] = Constants.CustomerNode;
+                        case "f" -> line_info[1] = Constants.StationNode;
+                        default -> line_info[1] = Constants.Depot;
+                    }
+                    // get the longitude
+                    start_tab = end_tab+1;
+                    end_tab = dataset_line.indexOf('\t',start_tab);
+                    information = dataset_line.substring(start_tab,end_tab);
+                    line_info[2] = Double.parseDouble(information);
+                    // get the latitude
+                    start_tab = end_tab+1;
+                    information = dataset_line.substring(start_tab);
+                    line_info[3] = Double.parseDouble(information);
+                    // add to arraylist
+                    list.add(line_info);
+                }
                 dataset_line = reader.readLine();
-                int first_tab = dataset_line.indexOf('\t');
-                String id =dataset_line.substring(0,first_tab);
-                System.out.println(id);
             }
             reader.close();
+            is_open =true;
         } catch (Exception e) {
             is_open = false;
             return;
+        }
+        if (is_open) {
+            ids = new HashMap<>();
+            for (int i =0;i<list.size();++i) {
+                ids.put((String)list.get(i)[0],(Constants) list.get(i)[1]);
+            }
+            System.out.println(ids.get("D"));
+
         }
 
 
