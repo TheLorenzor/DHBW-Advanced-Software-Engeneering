2d149dac19713636c07a23f1f4000e37903ecfd3
Tilmann <tilmann.lorenz@gmx.de>
2023-05-22 00:50:27 +0200

    will not work yet because stuff to do

--- src/main/java/gvrp/brute_force/BruteForceWorker.java
+++ src/main/java/gvrp/brute_force/BruteForceWorker.java
@@ -1,19 +1,33 @@
 package gvrp.brute_force;
 
 import data_import.DatasetLoader;
+import gvrp.shared.BestDistance;
 import gvrp.shared.Distance;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class BruteForceWorker implements Runnable {
     DatasetLoader dataset;
-    String [] route;
+    ArrayList<String> route;
 
-    BruteForceWorker(DatasetLoader dataset,String[] route) {
+    BruteForceWorker(DatasetLoader dataset, ArrayList<String> route) {
         this.dataset = dataset;
         this.route = route;
     }
         @Override
     public void run() {
-            Distance t = new Distance(route,dataset);
+            Object[] obj_arr = route.toArray();
+            String[] routes = new String[obj_arr.length];
+            for (int i =0;i<obj_arr.length;++i) {
+                routes[i] = (String) obj_arr[i];
+            }
+
+            Distance t = new Distance(routes,dataset);
+            double dist =t.getDistance();
+            if (BestDistance.update_distance(dist)) {
+                // put the whole write to file shit in here
+            }
 
     }
 }
--- src/main/java/gvrp/brute_force/Bruteforce.java
+++ src/main/java/gvrp/brute_force/Bruteforce.java
@@ -1,24 +1,29 @@
 package gvrp.brute_force;
 
 import data_import.DatasetLoader;
+import gvrp.shared.BestDistance;
 
 import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Bruteforce {
     DatasetLoader loader;
     String[] available_nodes;
     ArrayList<String> route;
     HashMap<String,Boolean> marked;
-
+    ThreadPoolExecutor executor;
 
     Bruteforce() {
         this.loader = new DatasetLoader();
         available_nodes =this.loader.getIDs();
         route = new ArrayList<>();
         marked = new HashMap<>();
+        int cores = Runtime.getRuntime().availableProcessors();
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(cores);
     }
 
     public void start_new_thread() {
@@ -29,6 +34,14 @@ public class Bruteforce {
 
 
     public int recursive_func (int current_try ) {
+        if (route.size()==available_nodes.length) {
+            start_new_thread();
+            BruteForceWorker worker = new BruteForceWorker(this.loader,route);
+            executor.submit(worker);
+            route.clear();
+            marked.clear();
+            return current_try==1000000000 ? -1 : current_try+1;
+        }
         for (String availableNode : available_nodes) {
             // if the element is already in the route it is supposed to be not marked
             if (marked.get(availableNode)!=null) {
@@ -36,17 +49,14 @@ public class Bruteforce {
             }
             route.add(availableNode);
             marked.put(availableNode,true);
-            int is_more = recursive_func(current_try);
-            if (is_more == -1) {
+            current_try = recursive_func(current_try);
+            if (current_try == -1) {
                 return -1;
-            } else {
-                current_try = is_more;
             }
         }
-        start_new_thread();
-        route.clear();
-        marked.clear();
-        return 0;
+        return current_try;
+
+
     }
 
 }
